c0c54c2eb2379faaebc51a8f5994331d65892ecc
Crowdbotics <team@crowdbotics.com>
2022-01-23 19:03:34 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,1 +1,33 @@
+from django.conf import settings
 from django.db import models
+
+
+class App(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    description = models.CharField(
+        max_length=500,
+    )
+    planid = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+
+
+class Plan(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    price = models.IntegerField(
+        null=True,
+        blank=True,
+    )
+
+
+class Subscription(models.Model):
+    "Generated Model"
+    planid = models.IntegerField()
+    active = models.BooleanField()
